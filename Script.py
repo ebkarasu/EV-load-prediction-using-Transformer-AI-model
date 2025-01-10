8e349b91d97bc0c5b6e59f808f669e44459bf6fe
ebkarasu <107682297+ebkarasu@users.noreply.github.com>
2025-01-11 00:53:58 +0300

    Script.py

--- Script.py
+++ Script.py
@@ -10,6 +10,9 @@ import math
 import copy
 import json
 import timedelta
+import matplotlib.pyplot as plt
+from sympy.stats.sampling.sample_numpy import numpy
+
 
 # This is a class for making Multi Head Attention processes and defined as a subclass of PyTorch's nn.Module
 class MultiHeadAttention(nn.Module):
@@ -239,11 +242,7 @@ monthToNumber = {
 # Assigning values to the connectTimes and disconnectTimes array
 for i in range(len(classArray)):
     connectTimes[i] = datetime.datetime(int(classArray[i].connectionTime[12:16]),monthToNumber.get(classArray[i].connectionTime[8:11]),int(classArray[i].connectionTime[5:7]),int(classArray[i].connectionTime[17:19]),int(classArray[i].connectionTime[20:22]))
-    disconnectTimes[i] = datetime.datetime(int(classArray[i].disconnectTime[12:16]),
-                                        monthToNumber.get(classArray[i].disconnectTime[8:11]),
-                                        int(classArray[i].disconnectTime[5:7]),
-                                        int(classArray[i].disconnectTime[17:19]),
-                                        int(classArray[i].disconnectTime[20:22]))
+    disconnectTimes[i] = datetime.datetime(int(classArray[i].disconnectTime[12:16]),monthToNumber.get(classArray[i].disconnectTime[8:11]),int(classArray[i].disconnectTime[5:7]),int(classArray[i].disconnectTime[17:19]),int(classArray[i].disconnectTime[20:22]))
 
 # Finding first starting point of the first charge connection in time
 minTime = datetime.datetime(min(connectTimes).year,min(connectTimes).month,min(connectTimes).day,min(connectTimes).hour)
@@ -254,38 +253,120 @@ dataSet = np.array([], dtype=float)
 flag = 0
 dummy = 0
 
+# Our dataset is going to show average power usage each hour of the entire time interval in the original data
 while(minTime < max(disconnectTimes)):
     for i in range(len(classArray)):
         if connectTimes[i] < minTime < disconnectTimes[i]:
             flag = 1
             dummy = i
-
     if flag:
         dataSet = np.append(dataSet, classArray[dummy].kWhDelivered / ((disconnectTimes[dummy] - connectTimes[dummy]).seconds / 3600.0))
     else:
         dataSet = np.append(dataSet, 0.0)
-
     flag = 0
     minTime = minTime + datetime.timedelta(minutes=60)
 
-dataSet = dataSet.round(decimals=3)
+# Converting our dataset to integers to be given transformer as an input
+dataSet = dataSet.round(decimals=2)
+dataSetInt = 100 * dataSet
+dataSetInt = numpy.int64(dataSetInt)
 
-print(dataSet,'\n',len(dataSet))
+# These arrays are to portionally train our transformer then to test it
+targetData = np.empty((64, 50),dtype=int)
+sourceData = np.empty((64, 50),dtype=int)
+
+# Filling target array with samples batchSize = 64
+for i in range(64):
+    targetData[i] = dataSetInt[10*i : 50 + 10*i]
+
+# Filling source array with samples batchSize = 64
+c = np.arange(1,len(dataSet))
+for i in range(64):
+    sourceData[i] = c[10*i : 50 + 10*i]
+
+# Dividing the target data into portions training, validating and testing
+targetData_training = targetData[:,0:35]
+targetData_validation = targetData[:,35:40]
+targetData_test = targetData[:,40:50]
+
+# Dividing the source data into portions training, validating and testing
+sourceData_training = sourceData[:,0:35]
+sourceData_validation = sourceData[:,35:40]
+sourceData_test = sourceData[:,40:50]
+
+# Transformer only accepts inputs as tensors which is a class in PyTorch
+sourceTensor_training = torch.from_numpy(sourceData_training)
+sourceTensor_validation = torch.from_numpy(sourceData_validation)
+sourceTensor_test = torch.from_numpy(sourceData_test)
+targetTensor_training = torch.from_numpy(targetData_training)
+targetTensor_validation = torch.from_numpy(targetData_validation)
+targetTensor_test = torch.from_numpy(targetData_test)
 
 # Determining specifications to be given into the transformer as an input
-embeddingDim = 512
-noHeads = 8
-noEncLayers = 2
-noDecLayers = 1
+embeddingDim = 512 # Embedding dimension
+noHeads = 8 # Number of heads
+noEncLayers = 6 # Number of encoding layers
+noDecLayers = 6 # Number of decoding layers
 FFND = 2048 # Feeding forward network dimension
 dropout = 0.05
-batchSize = 32
+
+# Creating transformer instance
+transformer = Transformer(1000, 1000, embeddingDim, noHeads, noEncLayers, FFND, 35, dropout)
 
 
+criterion = nn.CrossEntropyLoss(ignore_index=0) # Defines the loss function as cross-entropy loss. The ignore_index argument, also known as padding is set to zero.
+optimizer = optim.Adam(transformer.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-9) # Defines the optimizer as Adam with a learning rate of 0.0001 and specific beta values.
 
+transformer.train() # Sets the transformer model to training mode, enabling behaviors like dropout that only apply during training.
 
+print('Enter training epoches:')
+ep = int(input())
 
+# Iterating over 5 training epochs
+for epoch in range(ep):
+    optimizer.zero_grad() # Clears the gradients from the previous iteration
+    output = transformer(sourceTensor_training, targetTensor_training[:, :-1]) # Passes the source data and the target data through the transformer
+    loss = criterion(output.contiguous().view(-1, 1000), targetTensor_training[:, 1:].contiguous().view(-1)) # Computes the loss between the model's predictions and the target data.
+    loss.backward() # Computes the gradients of the loss with respect to the model's parameters
+    optimizer.step() # Updates the model's parameters using the computed gradients
+    print(f"Epoch: {epoch+1}, Loss: {loss.item()}") # Prints the current epoch number and the loss value for that epoch
 
+transformer.eval() # Puts the transformer model in evaluation mode, turns off certain behaviors like dropout that are only used during training
+
+with torch.no_grad(): # Disables gradient computation, as we don't need to compute gradients during validation
+
+    val_output = transformer(sourceTensor_test, targetTensor_test[:, :-1]) # Passes the validation source data and the validation target data through the transformer
+    val_loss = criterion(val_output.contiguous().view(-1, 1000), targetTensor_test[:, 1:].contiguous().view(-1)) # Computes the loss between the model's predictions and the validation target data
+    print(f"Validation Loss: {val_loss.item()}") # Prints the validation loss value
+
+# Creating and filling array to hold the predictions
+predictedDataSet = np.empty((64, 10),dtype=int) # Creating an array to hold the predictions
+for i in range(val_output.size()[0]):
+    for j in range(val_output.size()[1]):
+        predictedDataSet[i][j] = torch.argmax(val_output[i][j])
+
+# Since transformer operations exclude the first token in each sequence the last column is filled with zeros. To avoid discontinuity, we assign previous values to the zero column.
+predictedDataSet[:,9] = predictedDataSet[:,8]
+
+# Converting predicted array into 1-Dimensional
+predictedDataSet = predictedDataSet.reshape(-1)
+
+# We multiplied the original dataset by 100 to convert it into integers, now dividing by 100 to get original values
+predictedDataSet = np.array(predictedDataSet, dtype=float) / 100
+
+y = np.arange(1,748)
+y2  = np.arange(41,681)
+
+# Plotting original dataset and predicted ones to compare
+plt.rcParams["figure.figsize"] = [15, 5]
+plt.title("Average Power")
+plt.plot(y, dataSet, color="red")
+plt.plot(y2, predictedDataSet, color="blue")
+plt.legend(["Original", "Predicted (e="+str(ep)+")"], loc="lower right")
+plt.xlabel("Hours from May 9, 2020 to Jun 11, 2020 (747 total)")
+plt.ylabel("kW")
+plt.gca().yaxis.label.set(rotation='horizontal', ha='right');
+plt.show()
 
 
 
